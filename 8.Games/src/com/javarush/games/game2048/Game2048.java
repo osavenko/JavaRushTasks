07b5b2f0199fb7c4f03a26c9e28a10aa64a89299
osavenko <osavenko@gmail.com>
2019-08-06 17:57:38 +0300

    Game2048 step 7

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -2,5 +2,56 @@ package com.javarush.games.game2048;
 import com.javarush.engine.cell.*;
 
 public class Game2048 extends Game{
-
+    private static final int SIDE = 4;
+    private int[][] gameField = new int[SIDE][SIDE];
+    @Override
+    public void initialize() {
+        super.initialize();
+        setScreenSize(SIDE, SIDE);
+        createGame();
+        drawScene();
+    }
+    private void createGame(){
+        createNewNumber();
+        createNewNumber();
+    }
+    private void drawScene(){
+        for (int i = 0; i < SIDE; i++) {
+            for (int j = 0; j < SIDE; j++) {
+                int value = gameField[j][i];
+                setCellColoredNumber(i, j, value);
+            }
+        }
+    }
+    private void createNewNumber(){
+        int posX = getRandomNumber(SIDE);
+        int posY = getRandomNumber(SIDE);
+        int rnd = getRandomNumber(10);
+        while (gameField[posX][posY]!=0){
+            posX = getRandomNumber(SIDE);
+            posY = getRandomNumber(SIDE);
+        }
+        gameField[posX][posY] = rnd==9?4:2;
+    }
+    private Color getColorByValue(int value){
+        switch (value){
+            case 2: return Color.DARKBLUE;
+            case 4: return Color.BLUE;
+            case 8: return Color.YELLOW;
+            case 16: return Color.ALICEBLUE;
+            case 32: return Color.BEIGE;
+            case 64: return Color.BROWN;
+            case 128: return Color.CORAL;
+            case 256: return Color.CYAN;
+            case 512: return Color.DARKGRAY;
+            case 1024: return Color.DARKRED;
+            case 2048: return Color.RED;
+            default: return Color.AQUA;
+        }
+    }
+    private void setCellColoredNumber(int x, int y, int value){
+        Color color = getColorByValue(value);
+        setCellColor(x, y, color);
+        setCellValueEx(x, y, color ,value!=0?Integer.toString(value):"");
+    }
 }
