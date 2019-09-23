5b360077e186528f22ce9538ec2c7c468e21a8cf
osavenko <osavenko@gmail.com>
2019-09-23 22:54:11 +0300

    2311, 2401-2402

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+    public void printA(){
+        System.out.println("printA");
+    }
+    public void printB(){
+        System.out.println("printB");
+    }
+}
