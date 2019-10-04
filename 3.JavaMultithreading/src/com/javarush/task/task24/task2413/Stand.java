84576476718af7cd9687fb943d695ddc9d3b3f63
osavenko <osavenko@gmail.com>
2019-10-04 16:29:10 +0300

    Arkanoid 16

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -1,8 +1,25 @@
 package com.javarush.task.task24.task2413;
 
 public class Stand extends BaseObject{
+    private double speed;
+    private double direction;
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
     public Stand(double x, double y, double radius) {
         super(x, y, radius);
+    }
+
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        this.speed = 1;
+        this.direction = 0;
     }
 
     @Override
