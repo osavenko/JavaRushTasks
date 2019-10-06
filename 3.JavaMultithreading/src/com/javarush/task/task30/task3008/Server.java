026738cf5066a0f8e163d3653b679e43ba601261
osavenko <osavenko@gmail.com>
2019-10-06 10:54:33 +0300

    Chat 6

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,36 @@
 package com.javarush.task.task30.task3008;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    private static class Handler extends Thread{
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+
+        ConsoleHelper.writeMessage("Введите порт сервера: ");
+        int serverPort = ConsoleHelper.readInt();
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(serverPort);
+            ConsoleHelper.writeMessage("Сервер запущен...");
+            while (true) {
+                //Слушаем
+                Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                //запускаем handler
+                handler.start();
+            }
+        } catch (IOException e){
+
+        }finally {
+            serverSocket.close();
+        }
     }
 }
