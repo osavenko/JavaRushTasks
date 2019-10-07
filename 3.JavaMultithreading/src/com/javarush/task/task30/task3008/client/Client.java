84ba49f9cf85ecb60120f99d8332c99222d88e98
osavenko <osavenko@gmail.com>
2019-10-07 23:00:30 +0300

    Chat 15

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -12,7 +12,21 @@ public class Client {
     private volatile boolean clientConnected = false;
 
     public class SocketThread extends Thread {
-
+        protected void processIncomingMessage(String message){
+            ConsoleHelper.writeMessage(message);
+        }
+        protected void informAboutAddingNewUser(String userName){
+            ConsoleHelper.writeMessage("участник " + userName + " присоединился к чату");
+        }
+        protected void informAboutDeletingNewUser(String userName){
+            ConsoleHelper.writeMessage("участник " + userName + " покинул чат");
+        }
+        protected void notifyConnectionStatusChanged(boolean clientConnected){
+            Client.this.clientConnected = clientConnected;
+            synchronized (Client.this){
+                Client.this.notify();
+            }
+        }
     }
 
     protected String getServerAddress() {
