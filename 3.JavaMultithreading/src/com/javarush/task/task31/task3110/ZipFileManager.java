17fc64bde806e5a89eabb9616457b1cf2509f3c4
osavenko <osavenko@gmail.com>
2019-10-10 00:23:19 +0300

    archivator 2

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,6 +1,13 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
     private Path zipFile;
@@ -9,6 +16,17 @@ public class ZipFileManager {
         this.zipFile = zipFile;
     }
     public void createZip(Path source) throws Exception{
+
+        try(ZipOutputStream zip = new ZipOutputStream(Files.newOutputStream(zipFile))) {
+
+            ZipEntry ze = new ZipEntry(source.getFileName().toString());
+            zip.putNextEntry(ze);
+            try (InputStream is = Files.newInputStream(source)) {
+                while (is.available() > 0) {
+                    zip.write(is.read());
+                }
+            }
+        }
 
     }
 
