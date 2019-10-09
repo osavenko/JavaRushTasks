1acd962ecb2f4cde6f178b3227d26eb154ecc233
osavenko <osavenko@gmail.com>
2019-10-10 00:30:07 +0300

    archivator 3

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ConsoleHelper.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task31.task3110;
+
+import java.util.Scanner;
+
+public class ConsoleHelper {
+    public static void writeMessage(String message){
+        System.out.println(message);
+    }
+    public static String readString(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+    public static int readInt(){
+        return Integer.parseInt(readString());
+    }
+}
