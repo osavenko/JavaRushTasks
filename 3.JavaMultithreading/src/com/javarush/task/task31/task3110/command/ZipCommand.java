bad6c2440dfcff6d9ff197d1e1847e36a6224444
osavenko <osavenko@gmail.com>
2019-10-14 11:59:47 +0300

    archivator 12

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -1,4 +1,16 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public abstract class ZipCommand implements Command {
+    public ZipFileManager getZipFileManager() throws Exception{
+        ConsoleHelper.writeMessage("Введите полный путь к файлу:");
+        String sFullPath = ConsoleHelper.readString();
+        Path fullPath = Paths.get(sFullPath);
+        return new ZipFileManager(fullPath);
+    }
 }
