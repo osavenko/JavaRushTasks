4b19701a571a96f076c98af781b31e5884c979ea
osavenko <osavenko@gmail.com>
2019-10-14 11:44:30 +0300

    archivator 11

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
@@ -1,6 +1,5 @@
 package com.javarush.task.task31.task3110;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -12,24 +11,31 @@ public class FileManager {
     private Path rootPath;
     private List<Path> fileList;
 
-    public List<Path> getFileList() {
-        return fileList;
-
-    }
-
     public FileManager(Path rootPath) throws IOException {
         this.rootPath = rootPath;
-        fileList = new ArrayList<>();
+        this.fileList = new ArrayList<>();
         collectFileList(rootPath);
     }
+
+    public List<Path> getFileList() {
+        return fileList;
+    }
+
     private void collectFileList(Path path) throws IOException {
-        if(Files.isRegularFile(path)){
-            Path rel = rootPath.relativize(path);
-            fileList.add(rel);
-        }else if(Files.isDirectory(path)){
-            try(DirectoryStream<Path> ds = Files.newDirectoryStream(path)){
-                for(Path path1:ds)
-                    collectFileList(path1);
+        // Добавляем только файлы
+        if (Files.isRegularFile(path)) {
+            Path relativePath = rootPath.relativize(path);
+            fileList.add(relativePath);
+        }
+
+        // Добавляем содержимое директории
+        if (Files.isDirectory(path)) {
+            // Рекурсивно проходимся по всему содержмому директории
+            // Чтобы не писать код по вызову close для DirectoryStream, обернем вызов newDirectoryStream в try-with-resources
+            try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)) {
+                for (Path file : directoryStream) {
+                    collectFileList(file);
+                }
             }
         }
     }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,11 +1,14 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -16,18 +19,46 @@ public class ZipFileManager {
         this.zipFile = zipFile;
     }
     public void createZip(Path source) throws Exception{
+        Path zipDir = zipFile.getParent();
+        if(Files.notExists(zipDir))
+            Files.createDirectory(zipDir);
 
         try(ZipOutputStream zip = new ZipOutputStream(Files.newOutputStream(zipFile))) {
 
-            ZipEntry ze = new ZipEntry(source.getFileName().toString());
-            zip.putNextEntry(ze);
-            try (InputStream is = Files.newInputStream(source)) {
-                while (is.available() > 0) {
-                    zip.write(is.read());
-                }
+            if (Files.isDirectory(source)) {
+                FileManager fileManager = new FileManager(source);
+                List<Path> fileNames = fileManager.getFileList();
+
+                for (Path fileName : fileNames)
+                    addNewZipEntry(zip, source, fileName);
+
+            } else if (Files.isRegularFile(source)) {
+
+                addNewZipEntry(zip, source.getParent(), source.getFileName());
+            } else {
+
+                throw new PathIsNotFoundException();
             }
         }
 
     }
+    private void addNewZipEntry(ZipOutputStream zipOutputStream, Path filePath, Path fileName) throws Exception{
+        Path path = filePath.resolve(fileName);
+        try(InputStream inputStream = Files.newInputStream(path)){
+            ZipEntry zipEntry = new ZipEntry(fileName.toString());
+            zipOutputStream.putNextEntry(zipEntry);
+
+            copyData(inputStream,zipOutputStream);
+            zipOutputStream.closeEntry();
+        }
+    }
+    private void copyData(InputStream in, OutputStream out) throws Exception{
+        byte[] buffer = new byte[8 * 1024];
+        int len;
+        while ((len = in.read(buffer)) > 0) {
+            out.write(buffer, 0, len);
+        }
+    }
+
 
 }
