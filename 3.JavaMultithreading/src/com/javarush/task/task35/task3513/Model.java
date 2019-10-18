f1aa21b7db814a332b45847c4526f994696cd5da
osavenko <osavenko@gmail.com>
2019-10-19 00:29:23 +0300

    Game 2048_5

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class Model {
     private static final int FIELD_WIDTH = 4;
     private Tile[][] gameTiles;
+    int score;
+    int maxTile;
 
     public Model() {
         resetGameTiles();
@@ -33,6 +35,47 @@ public class Model {
         if(list.size()>0) {
             list.get((int)(list.size()*Math.random())).value = Math.random() < 0.9 ? 2 : 4;
         }
+    }
+    private boolean compressTiles(Tile[] tiles) {
+        boolean isChanged = false;
+        Tile temp;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (tiles[j].value == 0 && tiles[j + 1].value != 0) {
+                    temp = tiles[j];
+                    tiles[j] = tiles[j + 1];
+                    tiles[j + 1] = temp;
+                    isChanged = true;
+                }
+            }
+        }
+        return isChanged;
+    }
+    private boolean mergeTiles(Tile[] tiles){
+        boolean isChanged = false;
+        for (int j = 0; j < 3; j++) {
+            if (tiles[j].value != 0 && tiles[j].value == tiles[j + 1].value) {
+                tiles[j].value = tiles[j].value * 2;
+                tiles[j + 1].value = 0;
+                if (tiles[j].value > maxTile) maxTile = tiles[j].value;
+                score += tiles[j].value;
+                isChanged = true;
+
+            }
+        }
+
+        if (isChanged) {
+            Tile temp;
+            for (int j = 0; j < 3; j++) {
+                if (tiles[j].value == 0 && tiles[j + 1].value != 0) {
+                    temp = tiles[j];
+                    tiles[j] = tiles[j + 1];
+                    tiles[j + 1] = temp;
+                }
+            }
+        }
+
+        return isChanged;
     }
 
 }
