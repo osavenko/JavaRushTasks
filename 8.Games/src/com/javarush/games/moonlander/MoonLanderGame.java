3f739aec84761b9605c5a59a8c99fbb88b466825
osavenko <osavenko@gmail.com>
2019-10-29 21:27:52 +0200

    Moon 2

--- 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
+++ 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -7,6 +7,19 @@ public class MoonLanderGame extends Game {
 
     @Override
     public void initialize() {
+
         setScreenSize(WIDTH, HEIGHT);
+        createGame();
     }
+    private void createGame(){
+        drawScene();
+    }
+    private void drawScene(){
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                setCellColor(i, j, Color.AQUA);
+            }
+        }
+    }
+
 }
