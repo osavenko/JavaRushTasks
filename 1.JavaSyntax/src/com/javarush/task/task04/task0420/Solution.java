84b50b7b52fe8876576c364716440c321df5cb20
osavenko <osavenko@gmail.com>
2019-11-17 21:49:29 +0200

    0420

--- 1.JavaSyntax/src/com/javarush/task/task04/task0420/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0420/Solution.java
@@ -5,30 +5,32 @@ package com.javarush.task.task04.task0420;
 */
 
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        if ((a>b)&&(a>c)) {
-            if(b>c)
-                System.out.println(a + " " + b + " " + c);
-            else
-                System.out.println(a + " " + c + " " + b);
-        }
-/*        if ((b>a)&&(a>c)) {
-            System.out.println(a + " " + b + " " + c);
-        }else {
-            System.out.println(a + " " + b + " " + c);
-            if ((a>b)&&(b>c)) {
-                System.out.println(a + " " + b + " " + c);
-            }else {
-                System.out.println(a+" "+b+" "+c);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-*/
+        int[] vect = new int [3];
+
+        vect[0] = Integer.parseInt(br.readLine());
+        vect[1] = Integer.parseInt(br.readLine());
+        vect[2] = Integer.parseInt(br.readLine());
+
+        int k;
+        for (int i=0;i<vect.length;i++) {
+            for (int j=i; j<vect.length;j++) {
+                if (vect[j]>vect[i]) {
+                    k = vect[i];
+                    vect[i] = vect[j];
+                    vect[j] = k;
+                }
             }
+        }
+
+        for (int i=0;i<vect.length;i++) {
+            System.out.print(vect[i]+" ");
+        }
+    }
 }
