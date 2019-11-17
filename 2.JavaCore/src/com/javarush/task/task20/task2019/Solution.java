40af72ee2f3e12eb3d7dff2b197dffaee2b3a5da
osavenko <osavenko@gmail.com>
2019-11-17 19:39:57 +0200

    2019

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2019/Solution.java
@@ -0,0 +1,50 @@
+package com.javarush.task.task20.task2019;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Исправить ошибку. Сериализация
+*/
+public class Solution implements Serializable {
+
+    public static void main(String args[]) throws Exception {
+        String fName = "c:\\test.txt";
+        FileOutputStream fileOutput = new FileOutputStream(fName);
+        ObjectOutputStream outputStream = new ObjectOutputStream(fileOutput);
+
+        Solution solution = new Solution();
+        outputStream.writeObject(solution);
+
+        fileOutput.close();
+        outputStream.close();
+
+        //load
+        FileInputStream fiStream = new FileInputStream(fName);
+        ObjectInputStream objectStream = new ObjectInputStream(fiStream);
+
+        Solution loadedObject = (Solution) objectStream.readObject();
+
+        fiStream.close();
+        objectStream.close();
+
+        //Attention!!
+        System.out.println(loadedObject.size());
+    }
+
+    private Map<String, String> m = new HashMap<>();
+
+    public Map<String, String> getMap() {
+        return m;
+    }
+
+    public Solution() {
+        m.put("Mickey", "Mouse");
+        m.put("Mickey", "Mantle");
+    }
+
+    public int size() {
+        return m.size();
+    }
+}
