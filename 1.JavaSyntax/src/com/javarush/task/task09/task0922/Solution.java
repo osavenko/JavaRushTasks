c0b9f180f0cd3ac1d6639ed569f6c503ddfa0e4c
osavenko <osavenko@gmail.com>
2019-11-27 22:22:17 +0200

    0922

--- 1.JavaSyntax/src/com/javarush/task/task09/task0922/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0922/Solution.java
@@ -14,5 +14,11 @@ public class Solution {
 
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        SimpleDateFormat newFormat = new SimpleDateFormat("MMM dd, yyyy",Locale.ENGLISH);
+        Date date = format.parse(r.readLine());
+        String mdy = newFormat.format(date);
+        System.out.println(mdy.toUpperCase());
     }
 }
