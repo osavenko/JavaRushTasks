20e0635296e2f569bcf8c820e4aebd2a4b1bd8a6
osavenko <osavenko@gmail.com>
2019-11-27 22:00:41 +0200

    0827

--- 1.JavaSyntax/src/com/javarush/task/task08/task0827/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0827/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task08.task0827;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /* 
@@ -8,10 +9,15 @@ import java.util.Date;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(isDateOdd("MAY 1 2013"));
+        System.out.println(isDateOdd("MAY 2 2013"));
     }
 
     public static boolean isDateOdd(String date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date(date));
+
+        int n = cal.get(Calendar.DAY_OF_YEAR);
+        if(n%2==0) return false;
         return true;
     }
 }
