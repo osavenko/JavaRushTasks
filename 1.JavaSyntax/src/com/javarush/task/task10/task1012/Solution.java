a81cfedd692867d5259560be63a968073eee3cd6
osavenko <osavenko@gmail.com>
2019-11-28 19:34:37 +0200

    1012-1014

---
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1012/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task10.task1012;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/* 
+Количество букв
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        // Алфавит
+        String abc = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        char[] abcArray = abc.toCharArray();
+        HashMap<Character,Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < abcArray.length; i++) {
+            hashMap.put(abcArray[i], 0);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            String s = reader.readLine();
+            char[] chars = s.toLowerCase().toCharArray();
+            for (int j = 0; j < chars.length; j++) {
+                if (hashMap.containsKey(chars[j]))
+                    hashMap.put(chars[j], hashMap.get(chars[j]) + 1);
+            }
+        }
+
+        for (int i = 0; i < abcArray.length; i++) {
+            System.out.println(abcArray[i] + " " + hashMap.get(abcArray[i]));
+        }
+
+    }
+}
