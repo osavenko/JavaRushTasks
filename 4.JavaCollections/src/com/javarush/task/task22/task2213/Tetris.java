1183d9cb82d4c6c0240cca4786d1d6d8706cd9d3
osavenko <osavenko@gmail.com>
2019-12-01 16:22:22 +0200

    2213

---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task22.task2213;
+
+public class Tetris {
+    private Field field;
+    private Figure figure;
+
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+    public static Tetris game;
+    public static void main(String[] args) {
+        game = new Tetris();
+        game.run();
+    }
+    public void run(){
+
+    }
+    public void step(){
+        
+    }
+}
