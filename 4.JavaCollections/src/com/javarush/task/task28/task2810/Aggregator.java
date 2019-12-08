9784712edae75bdd1b91bfd2d576b46895a08966
osavenko <osavenko@gmail.com>
2019-12-08 17:34:08 +0200

    2810_12 agregator

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -1,12 +1,19 @@
 package com.javarush.task.task28.task2810;
 
 import com.javarush.task.task28.task2810.model.HHStrategy;
+import com.javarush.task.task28.task2810.model.Model;
 import com.javarush.task.task28.task2810.model.Provider;
+import com.javarush.task.task28.task2810.view.HtmlView;
 
 import java.io.IOException;
 
 public class Aggregator {
     public static void main(String[] args) throws IOException {
+        HtmlView htmlView = new HtmlView();
+        Model model = new Model(htmlView, new Provider(new HHStrategy()));
+        Controller controller = new Controller(model);
+        htmlView.setController(controller);
 
+        htmlView.userCitySelectEmulationMethod();
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -3,6 +3,7 @@ package com.javarush.task.task28.task2810.view;
 import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
 
+import java.io.IOException;
 import java.util.List;
 
 public class HtmlView implements View {
@@ -10,11 +11,15 @@ public class HtmlView implements View {
 
     @Override
     public void update(List<Vacancy> vacancies) {
-
+        System.out.println(vacancies.size());
     }
 
     @Override
     public void setController(Controller controller) {
         this.controller = controller;
     }
+    public void userCitySelectEmulationMethod() throws IOException {
+        controller.onCitySelect("Odessa");
+    }
+
 }
