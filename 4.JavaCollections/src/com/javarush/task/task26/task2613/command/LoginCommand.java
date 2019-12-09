6b8568d7c3121028da4b4ff396601a298e663781
osavenko <osavenko@gmail.com>
2019-12-09 20:08:29 +0200

    2613_12

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/command/LoginCommand.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/LoginCommand.java
@@ -7,40 +7,37 @@ import com.javarush.task.task26.task2613.exception.InterruptOperationException;
 import java.util.ResourceBundle;
 
 public class LoginCommand implements Command {
+    private ResourceBundle validCreditCards = ResourceBundle.getBundle(CashMachine.class.getPackage().getName()
+            + ".resources.verifiedCards");
+
+
     @Override
     public void execute() throws InterruptOperationException {
-        String hardCardNumber = "123456789012";
-        String hardPin = "1234";
-
         ConsoleHelper.writeMessage("Enter card number - 12 digit.");
         String cardNumber = ConsoleHelper.readString();
 
         ConsoleHelper.writeMessage("Enter card pin code - 4 digit");
         String pin = ConsoleHelper.readString();
-
         while (true) {
             if (isInvalidCardNumber(cardNumber)) {
                 ConsoleHelper.writeMessage("Invalid card number. Try again. Enter card number - 12 digit:");
                 cardNumber = ConsoleHelper.readString();
-
             } else if (isInvalidPinCode(pin)) {
                 ConsoleHelper.writeMessage("Invalid card pin code. Try again. Enter card pin code - 4 digit:");
                 pin = ConsoleHelper.readString();
 
-            } else {
-                if ( ! cardNumber.equals(hardCardNumber)) {
-                    ConsoleHelper.writeMessage("Enter hard card number - 123456789012");
+            } else{
+                if ( ! this.validCreditCards.containsKey(cardNumber)) {
+                    ConsoleHelper.writeMessage("Card number is not found. Try again:");
                     cardNumber = ConsoleHelper.readString();
                     continue;
 
-                } else if (! pin.equals(hardPin)) {
-                    ConsoleHelper.writeMessage("Enter hard card pin code - 1234");
+                } else if ( ! pin.equals(this.validCreditCards.getString(cardNumber) ) ) {
+                    ConsoleHelper.writeMessage("Pin code is wrong. Enter pin code again:");
                     pin = ConsoleHelper.readString();
                     continue;
-
-                } else {
-                    ConsoleHelper.writeMessage("Verification success.");
                 }
+                ConsoleHelper.writeMessage("Verification success.");
                 break;
             }
         }
