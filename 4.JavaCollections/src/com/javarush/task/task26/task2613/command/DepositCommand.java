478a07d17e145e43d3c5f04a19a189d704e893fc
osavenko <osavenko@gmail.com>
2019-12-09 09:04:02 +0200

    2613_7

---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/DepositCommand.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task26.task2613.command;
+
+import com.javarush.task.task26.task2613.exception.InterruptOperationException;
+
+class DepositCommand implements Command {
+    @Override
+    public void execute() throws InterruptOperationException {
+
+    }
+}
